7bf230d197ed75736026bb920d717182d7b387d7
TimotheeGimbert <timothee.gimbert@free.fr>
2022-02-24 16:26:32 +0100

    initial commit

---
+++ build/precache-manifest.0db6adac930207f89048d2f163b1727d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a51138cf5f030ec8cc25d48dafa50be",
+    "url": "/index.html"
+  },
+  {
+    "revision": "05c1702a3fdad67f03e2",
+    "url": "/static/css/2.929389fa.chunk.css"
+  },
+  {
+    "revision": "5dac874bf253841a63c7",
+    "url": "/static/css/main.5db5b2d9.chunk.css"
+  },
+  {
+    "revision": "05c1702a3fdad67f03e2",
+    "url": "/static/js/2.734f1992.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.734f1992.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5dac874bf253841a63c7",
+    "url": "/static/js/main.78c3b6ac.chunk.js"
+  },
+  {
+    "revision": "4d7976a7f7689729d64f",
+    "url": "/static/js/runtime-main.3904f7be.js"
+  }
+]);
